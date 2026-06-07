ac8f0670137f442076886979e12fcdae16991c0b
Hemashri Bellary <hemashrirbellary@gmail.com>
2026-06-07 13:59:55 +0530

    add pr2.py

---
+++ pr2.py
@@ -0,0 +1,6 @@
+name = 'hema'
+target_hours = 25
+
+studied_today = int(input('How many hours did you study today? '))
+remaining_hours = target_hours - studied_today
+print('student',name,'has',remaining_hours,'hours left to study to reach the target of',target_hours,'hours.')
