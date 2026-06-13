cf3341fa7401cec41bb7265989288b77806494b6
Hemashri Bellary <hemashrirbellary@gmail.com>
2026-06-13 18:09:00 +0530

    odd even

---
+++ pr9.py
@@ -0,0 +1,8 @@
+print('odd or even checker')
+num = range(1,11)
+for i in num:
+    if i % 2 == 0:
+        print(i, "is even")
+    else:
+        print(i, "is odd")
+print('done checking odd and even numbers')
