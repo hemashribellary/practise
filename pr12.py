a7caa565a3414b26d02343864200321c21819c19
Hemashri Bellary <hemashrirbellary@gmail.com>
2026-06-14 14:50:40 +0530

    pc

---
+++ pr12.py
@@ -0,0 +1,8 @@
+print('password checker')
+c_pass = 'python123'
+u_pass = input('enter your password: ')
+if u_pass == c_pass:
+    print('access granted.')  
+else:
+    print('access denied.')
+     
